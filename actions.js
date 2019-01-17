337ac26dedf8b10940096f4e1caa686ac470e194
Linda <31568225+lindasown@users.noreply.github.com>
2019-01-17 16:14:58 +0100

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,46 +1,90 @@
 function ACTIONS() {
 	var that			= this;
-	this.actualPosition	= [8, 2];
-	this.direction		= '1';
-
-
 
 	that.init = function() {
 		console.log('ACTIONS ready');
-		that.placeRobot(that.actualPosition);
-		that.test();
+		robotid = 'robot-1';
+		
+		//that.test();
+		that.executeRobotActions(robotid);
+	}
+
+	that.getPosition = function(robotid) {
+		return robotlist[robotid]['position']['coordinates'];
+	}
+
+	that.getDirection = function(robotid) {
+		return robotlist[robotid]['position']['direction'];
 	}
 
 	that.test = function() {
+		var robotid = 'robot-1';
 		$(document).on('keyup', function(e) {
 			if (e.which === 38) {
-				that.moveRobot(1);
+				that.moveRobot(robotid, 1);
 			} else if (e.which === 76) {
-				that.turnRobot('left', 1);
+				that.turnRobot(robotid, 'left', 1);
 			} else if (e.which === 82) {
-				that.turnRobot('right', 1);
+				that.turnRobot(robotid, 'right', 1);
 			} else if (e.which === 85) {
-				that.turnRobot('right', 2);
+				that.turnRobot(robotid, 'right', 2);
 			}
 		})	
 	}
 
-	that.placeRobot = function(positionXY) {
-		var actualFieldNumber = positionXY[1] * 10;
-		actualFieldNumber = actualFieldNumber + positionXY[0]
-		$('#playboard > div:nth-child(' + actualFieldNumber + ')').append('<div class="robot"></div>');
-		$('.robot').attr('direction', that.direction);
+	that.executeRobotActions = function(robotid) {
+		$(document).on('keyup', function(e) {
+			if (e.which === 13) {
+				setTimeout(function(){ that.getSingleRobotAction(robotid, 1); }, 200);
+			} 
+		})
 	}
 
-	that.replaceRobot = function(positionXY) {
+	that.getSingleRobotAction = function(robotid, counter) {
+		var actualAction = $('#robotControl .turn' + counter + ' select').val();
+
+		if (actualAction == '1-forward') {
+			that.moveRobot(robotid, 1);
+		} else if (actualAction == '2-forward') {
+			that.moveRobot(robotid, 2);
+		} else if (actualAction == '3-forward') {
+			that.moveRobot(robotid, 3);
+		} else if (actualAction == '1-back') {
+			that.moveRobot(robotid, -1);
+		} else if (actualAction == 'turn-left') {
+			that.turnRobot(robotid, 'left', 1);
+		} else if (actualAction == 'turn-right') {
+			that.turnRobot(robotid, 'right', 1);
+		} else if (actualAction == 'u-turn') {
+			that.uTurnRobot(robotid);
+		} 
+
+		if (counter <= 5) {
+			setTimeout(function(){ that.getSingleRobotAction(robotid, counter); }, 500);
+			counter++;
+		}
+	}
+
+	that.robotOptic = function(robotid) {
+		$("#" + robotid).css('background-image', 'url(' + robotlist[robotid]['information']['image'] + ')');
+	}
+
+	that.placeRobot = function(robotid, positionXY) {
+		var actualFieldNumber = that.getPosition(robotid)[1] * 10;
+		
+		actualFieldNumber = actualFieldNumber + positionXY[0];
+		$('#playboard > div:nth-child(' + actualFieldNumber + ')').append('<div class="robot" id="' + robotid + '"></div>');
+		$('.robot').attr('direction', that.getDirection(robotid));
+		that.robotOptic(robotid);
+	}
+
+	that.replaceRobot = function(robotid, positionXY) {
 		$('.robot').remove();
-
-		that.placeRobot(positionXY);
-		that.actualPosition = positionXY;
+		that.placeRobot(robotid, positionXY);	
 	}
 
-	that.turnRobot = function(direction, steps) {
-		var actualDirection = that.direction; //get actualRobotDirection 
+	that.turnRobot = function(robotid, direction, steps) {
+		var actualDirection = that.getDirection(robotid); //get actualRobotDirection 
 		var newDirection = actualDirection;
 		console.log(actualDirection);
 		if (direction === 'left') {
@@ -54,21 +98,19 @@ function ACTIONS() {
 				newDirection = parseInt(newDirection) - 4;
 			}
 		}
-		that.direction = newDirection;
+		robotlist[robotid]['position']['direction'] = newDirection;
 		$('.robot').attr('direction', newDirection);
-		console.log(that.direction);
 	}
 
-	that.uTurnRobot = function() {
-		that.turnRobot('left', 2);
+	that.uTurnRobot = function(robotid) {
+		that.turnRobot(robotid, 'left', 2);
 	}
 
-
-	that.moveRobot = function(steps, direction = 'default') {
+	that.moveRobot = function(robotid, steps, direction = 'default') {
 		var newPosition		= [];
-		newPosition[0]		= that.actualPosition[0];
-		newPosition[1]		= that.actualPosition[1];
-		var robotDirection	= that.direction;
+		newPosition[0]		= that.getPosition(robotid)[0];
+		newPosition[1]		= that.getPosition(robotid)[1];
+		var robotDirection	= that.getDirection(robotid);
 
 		if (direction === 'default') {
 			if (robotDirection == '1') {
@@ -91,11 +133,7 @@ function ACTIONS() {
 				newPosition[0] = newPosition[0] - steps;
 			}  
 		}
-
-		console.log(newPosition);
-		that.replaceRobot(newPosition);
+		robotlist[robotid]['position']['coordinates'] = newPosition;
+		that.replaceRobot(robotid, newPosition);	
 	}
-
-
-
 }

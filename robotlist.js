c9e3cc3944609943c6f0077e121a196949491fa1
Linda <31568225+lindasown@users.noreply.github.com>
2019-01-17 16:16:14 +0100

    Add files via upload

---
+++ robotlist.js
@@ -0,0 +1,21 @@
+robotlist = {
+	"robot-1" : {
+		"extensions" : {
+			"extension1" : "extensionID",
+			"extension2" : "extensionID"
+		}, 
+		"damage" : {
+			"spam" : 10,
+			"virus" : 3
+		},
+		"position" : {
+			"direction" : "2",
+			"coordinates" : [3,5]
+		},
+		"information" : {
+			"name" : "Radio Bot", 
+			"image" : "https://cdn.dribbble.com/users/146798/screenshots/2398320/r2d2.png"
+
+		}
+	}
+}
